0a125a450a782dd3738a19e55b3e889f7389f1cc
S.Rebouh <rebouh.sarah@gmail.com>
2017-01-10 11:15:17 +0100

    Test ouverture dossiers

--- file-explore.php
+++ file-explore.php
@@ -13,25 +13,53 @@ $scan = scandir('/home/sarahr/');
       {
           echo $file.'<br>';
       }
-      else{
+      else {
+          
         echo $file.'<br>';
       }
   }
 
 //--------- Fonction à appeler pour chercher dans un dossier précis et sortir les fichiers
 
-$scan = scandir('/home/sarahr/Formulaire/');
+$scan = scandir('/home/sarahr/jeu-de-la-fourchette/');
 
   foreach($scan as $file)
   {
-      if (!is_dir("/home/sarahr/Formulaire/$file"))
+      if (!is_dir("/home/sarahr/jeu-de-la-fourchette/$file"))
       {
           echo $file.'<br>';
       }
-      else{
+      
+      else {
+          
         echo $file.'<br>';
+          
       }
   }
 
+//---------------- Fonction pour scanner les dossiers
+
+$vrar = $_GET['/home/sarahr/'];
+
+if(strlen($var)==0){
+
+$var='.';
+
+}
+
+$ar = scandir($var);
+
+while (list ($key, $val) = each ($ar)) {
+
+if (is_dir($val)){
+echo "<a href=scandir.php?/home/sarahr/=$val>$val</a><br>";
+
+}else {
+
+echo "$val<br>";
+
+}
+
+}
 
 ?>
