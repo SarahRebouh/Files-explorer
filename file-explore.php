90f8abf8e7e76c0e97891a42a444ee32b04e5847
S.Rebouh <rebouh.sarah@gmail.com>
2017-01-10 15:18:17 +0100

    Auto stash for revert of "Test ouverture dossiers"

--- file-explore.php
+++ file-explore.php
@@ -1,20 +1,50 @@
 <?php
  
-    echo $_SERVER['DOCUMENT_ROOT'];
-    echo "<br>";
+// -------------------------- TEST
+//-------------- Fonction qui cherche les dossiers et les affiche
+$scan = scandir('/home/sarahr/');
 
-    $dir = "/home/sarahr/";
-
-// Open a directory, and read its contents
-
-    if (is_dir($dir)){
-    if ($dh = opendir($dir)){
-        while (($file = readdir($dh)) !== false){
-            echo "filename:" . $file . "<br>";
-    }
-    closedir($dh);
+  foreach($scan as $file)
+  {
+      if (is_dir("/home/sarahr/$file"))
+      {
+          echo '<img src="images/dossier.png">'. $file.'<br>';
+      }
+      else {
+          
+        echo '<img src="images/file.png">'. $file.'<br>';
+      }
   }
-}
 
-?>
+
+//--------- Fonction à appeler pour chercher dans un dossier précis et sortir les fichiers
+
+//
+//$scan = scandir('/home/sarahr/jeu-de-la-fourchette/');
+//  foreach($scan as $file)
+//  {
+//      if (!is_dir("/home/sarahr/jeu-de-la-fourchette/$file"))
+//      {
+//          echo $file.'<br>';
+//      }
+//      
+//      else {
+//          
+//        echo $file.'<br>';
+//          
+//      }
+//  }
+//---------------- Fonction pour scanner les dossiers
+//$vrar = $_GET['/home/sarahr/'];
+//if(strlen($var)==0){
+//$var='.';
+//}
+//$ar = scandir($var);
+//while (list ($key, $val) = each ($ar)) {
+//if (is_dir($val)){
+//echo "<a href=scandir.php?/home/sarahr/=$val>$val</a><br>";
+//}else {
+//echo "$val<br>";
+//}
+//}
 
--- index.php
+++ index.php
@@ -16,28 +16,30 @@
         <h1 class=titre>INDEX</h1>
     </header>
     
-<main class=container>
-   
-    <div class="all col-lg-12">
-        <div class="doss col-lg-6">
-            <h2>► Dossiers</h2>
+    <main class=container>
+
+        <div class="all col-lg-12">
+            <div class="doss col-lg-6">
+                <h2>► Dossiers</h2>
+            </div>
+
+            <div class="fich col-lg-6">
+                <h2>► Fichiers</h2>
+            </div>
         </div>
 
-        <div class="fich col-lg-6">
-            <h2>► Fichiers</h2>
+        <div class="container ok col-lg-12">
+            <div class="corpsdoss container col-lg-6">
+                <?php include("file-explore.php"); ?>
+            </div>
+
+            <div class="corpsfich container col-lg-6">
+                <?php include("file-explore.php"); ?>
+            </div>
         </div>
-    </div>
+
+    </main>
     
-    <div class="container ok col-lg-12">
-        <div class="corpsdoss container col-lg-6">
-            <?php include("file-explore.php"); ?>
-        </div>
-        
-        <div class="corpsfich container col-lg-6">
-            <?php include("file-explore.php"); ?>
-        </div>
-    </div>
-    
-</main>
+</body>
 
 </html>
